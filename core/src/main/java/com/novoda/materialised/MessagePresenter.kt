43bbaab2c660f0dd8e473dbb7636542fa38acfb9
Joe Timmins <joe@novoda.com>
2016-06-06 16:45:12 +0100

    .gitignore ignores vim swaps, core module has kotlin

---
+++ core/src/main/java/com/novoda/materialised/MessagePresenter.kt
@@ -0,0 +1,18 @@
+package com.novoda.materialised
+
+class MessagePresenter(toggleMessages: ToggleMessages) {
+    private val toggleMessages: ToggleMessages = toggleMessages
+    private var isToggled = false
+
+    private fun shouldShowDefaultMessage(): Boolean {
+        val isCurrentlyToggled = isToggled
+
+        isToggled = !isToggled
+
+        return isCurrentlyToggled
+    }
+
+    fun currentMessage(): String {
+        return if (shouldShowDefaultMessage()) toggleMessages.defaultMessage else toggleMessages.toggledMessage
+    }
+}
