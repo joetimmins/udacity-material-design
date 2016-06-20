001d557ea48dc674b500c9023f02aace0dd4f13a
Joe Timmins <joe@novoda.com>
2016-06-20 10:21:38 +0100

    Using data binding util instead of findViewById

--- app/src/main/java/com/novoda/materialised/MainActivity.java
+++ app/src/main/java/com/novoda/materialised/MainActivity.java
@@ -1,10 +1,9 @@
 package com.novoda.materialised;
 
+import android.databinding.DataBindingUtil;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,6 +12,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.novoda.materialised.databinding.MainActivityBinding;
 import com.novoda.materialised.example.MessagePresenter;
 import com.novoda.materialised.example.ToggleMessages;
 import com.novoda.materialised.hackernews.StoryViewModel;
@@ -23,41 +23,36 @@ public final class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        MainActivityBinding mainActivityLayout = DataBindingUtil.setContentView(this, R.layout.main_activity);
+
+        setSupportActionBar(mainActivityLayout.toolbar);
+
+        mainActivityLayout.fab.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                            Toast.makeText(MainActivity.this, "This button does nothing", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
 
         final MessagePresenter messagePresenter = new MessagePresenter(new ToggleMessages("hello world!", "hello again!"));
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        if (fab != null) {
-            fab.setOnClickListener(
-                    new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                                Toast.makeText(MainActivity.this, "This button does nothing", Toast.LENGTH_SHORT).show();
-                            }
+        mainActivityLayout.helloWorldButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                            int finalRadius = (int) Math.hypot(view.getWidth() / 2, view.getHeight() / 2);
+                            ((Button) view).setText(messagePresenter.currentMessage());
+                            ViewAnimationUtils.createCircularReveal(view, view.getWidth() / 2, view.getHeight() / 2, 0, finalRadius).start();
                         }
                     }
-            );
-        }
-
-        final Button helloWorld = (Button) findViewById(R.id.hello_world_button);
-        if (helloWorld != null) {
-            helloWorld.setOnClickListener(
-                    new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                                int finalRadius = (int) Math.hypot(view.getWidth() / 2, view.getHeight() / 2);
-                                ((Button) view).setText(messagePresenter.currentMessage());
-                                ViewAnimationUtils.createCircularReveal(view, view.getWidth() / 2, view.getHeight() / 2, 0, finalRadius).start();
-                            }
-                        }
-                    }
-            );
-        }
+                }
+        );
     }
 
     @Override
--- core/src/main/java/com/novoda/materialised/example/MessagePresenter.kt
+++ core/src/main/java/com/novoda/materialised/example/MessagePresenter.kt
@@ -4,15 +4,15 @@ class MessagePresenter(toggleMessages: ToggleMessages) {
     private val toggleMessages: ToggleMessages = toggleMessages
     private var isToggled = false
 
+    fun currentMessage(): String {
+        return if (shouldShowDefaultMessage()) toggleMessages.defaultMessage else toggleMessages.toggledMessage
+    }
+
     private fun shouldShowDefaultMessage(): Boolean {
         val isCurrentlyToggled = isToggled
 
         isToggled = !isToggled
 
         return isCurrentlyToggled
-    }
-
-    fun currentMessage(): String {
-        return if (shouldShowDefaultMessage()) toggleMessages.defaultMessage else toggleMessages.toggledMessage
     }
 }
