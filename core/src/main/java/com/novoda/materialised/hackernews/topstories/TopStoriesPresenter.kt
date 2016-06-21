2a79524df4fb09eebf02425f026b04d46791fef7
Joe Timmins <joe@novoda.com>
2016-06-21 10:53:20 +0100

    TopStoriesPresenter looks much more Kotlin-y

--- core/src/main/java/com/novoda/materialised/hackernews/topstories/TopStoriesPresenter.kt
+++ core/src/main/java/com/novoda/materialised/hackernews/topstories/TopStoriesPresenter.kt
@@ -1,20 +1,20 @@
 package com.novoda.materialised.hackernews.topstories
 
-import com.novoda.materialised.hackernews.ValueCallback
 import com.novoda.materialised.hackernews.items.ItemsDatabase
 import com.novoda.materialised.hackernews.items.StoryViewModel
+import com.novoda.materialised.hackernews.valueCallbackFor
 
 class TopStoriesPresenter(val topStoriesDatabase: TopStoriesDatabase, val itemsDatabase: ItemsDatabase, val storyView: StoryView) {
     fun startPresenting() {
         topStoriesDatabase.readAll(
-                object : ValueCallback<List<Long>> {
-                    override fun onValueRetrieved(value: List<Long>) {
-                        itemsDatabase.readItem(value[0].toInt(), object : ValueCallback<StoryViewModel> {
-                            override fun onValueRetrieved(value: StoryViewModel) {
-                                storyView.updateWith(value)
+                valueCallbackFor<List<Long>> {
+                    itemsDatabase.readItem(
+                            it[0].toInt(),
+                            valueCallbackFor<StoryViewModel> {
+                                storyView.updateWith(it)
                             }
-                        })
-                    }
-                })
+                    )
+                }
+        )
     }
 }
