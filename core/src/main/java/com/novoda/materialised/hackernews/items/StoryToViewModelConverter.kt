eadb139583837846af6b43483e43b10459a02820
Joe Timmins <joe@novoda.com>
2016-06-27 11:51:11 +0100

    inlined a method

--- core/src/main/java/com/novoda/materialised/hackernews/items/StoryToViewModelConverter.kt
+++ core/src/main/java/com/novoda/materialised/hackernews/items/StoryToViewModelConverter.kt
@@ -1,6 +1,4 @@
 package com.novoda.materialised.hackernews.items
 
-fun convertStoryToViewModel(story: Story): StoryViewModel {
-    return StoryViewModel(story.by, story.kids, story.id, story.score, story.title, story.url)
-}
+fun convertStoryToViewModel(story: Story): StoryViewModel = StoryViewModel(story.by, story.kids, story.id, story.score, story.title, story.url)
 
