6baef1f7953f562f566db11edf0f4d0d178f08a8
Joe Timmins <joe@novoda.com>
2016-07-01 13:31:12 +0100

    Cleaned up the click listeners a bit

--- core/src/main/java/com/novoda/materialised/hackernews/ClickListener.kt
+++ core/src/main/java/com/novoda/materialised/hackernews/ClickListener.kt
@@ -3,3 +3,9 @@ package com.novoda.materialised.hackernews
 interface ClickListener<T : ViewModel> {
     fun onClick(viewModel: T)
 }
+
+class NoOpClickListener<T : ViewModel> : ClickListener<T> {
+    override fun onClick(viewModel: T) {
+        // No-op
+    }
+}
