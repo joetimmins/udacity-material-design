9a0e2f0a550ccd9a18fb0b459ac588be33c15789
Joe Timmins <joe@novoda.com>
2016-07-03 18:51:32 +0100

    No need for ViewModelWithClickListener

--- core/src/main/java/com/novoda/materialised/hackernews/ClickListenerBuilder.kt
+++
@@ -1,5 +0,0 @@
-package com.novoda.materialised.hackernews
-
-fun <T : ViewModel<T>> addNoOpClickListeners(viewModels: List<T>): List<ViewModelWithClickListener<T>> {
-    return viewModels.map { viewModel -> ViewModelWithClickListener(viewModel, NoOpClickListener()) }
-}
--- core/src/main/java/com/novoda/materialised/hackernews/ViewModelWithClickListener.kt
+++
@@ -1,6 +0,0 @@
-package com.novoda.materialised.hackernews
-
-data class ViewModelWithClickListener<T : ViewModel<T>>(
-        val viewModel: T,
-        val clickListener: ClickListener<T>
-) {}
