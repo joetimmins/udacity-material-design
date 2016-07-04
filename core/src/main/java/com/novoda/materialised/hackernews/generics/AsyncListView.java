455f918552ef9bb907edd71b366bdf3f1642f2cb
Joe Timmins <joe@novoda.com>
2016-07-04 15:31:10 +0100

    Deleted all the the old ViewModel stuff

--- core/src/main/java/com/novoda/materialised/hackernews/generics/AsyncListView.java
+++
@@ -1,8 +0,0 @@
-package com.novoda.materialised.hackernews.generics;
-
-import java.util.List;
-
-public interface AsyncListView<T extends ViewModel<T>> {
-    void updateWith(List<T> viewModels);
-    void updateWith(T viewModel);
-}
--- core/src/main/java/com/novoda/materialised/hackernews/generics/ViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.novoda.materialised.hackernews.generics
-
-interface ViewModel<T> {
-    val id: Int
-    val clickListener: ClickListener<T>
-}
