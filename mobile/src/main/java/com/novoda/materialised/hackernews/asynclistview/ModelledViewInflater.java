b9463d5ebc7daa88891239bc8a9b151b10ae6912
Joe Timmins <joe@novoda.com>
2016-10-11 17:16:13 +0100

    Remove unnecessary return statement in ModelledViewInflater

--- mobile/src/main/java/com/novoda/materialised/hackernews/asynclistview/ModelledViewInflater.java
+++ mobile/src/main/java/com/novoda/materialised/hackernews/asynclistview/ModelledViewInflater.java
@@ -9,20 +9,18 @@ import java.lang.reflect.Constructor;
 final class ModelledViewInflater<V extends View & ModelledView<? extends ViewModel>> {
     private final Class<V> viewClass;
 
-    public ModelledViewInflater(Class<V> viewClass) {
+    ModelledViewInflater(Class<V> viewClass) {
         this.viewClass = viewClass;
     }
 
     V inflateUsing(ViewGroup parent) {
         Constructor<V> constructor;
-        V result;
         try {
             constructor = viewClass.getDeclaredConstructor(Context.class);
-            result = constructor.newInstance(parent.getContext());
+            return constructor.newInstance(parent.getContext());
         } catch (Exception e) { // this is horrendous, but multi-catching gives a compile error on API < 19
             e.printStackTrace();
             throw new RuntimeException("Couldn't inflate ModelledView instance!");
         }
-        return result;
     }
 }
