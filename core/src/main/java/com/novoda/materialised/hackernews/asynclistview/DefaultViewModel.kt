73b7e0e6680fe8fff0e695c9f935ac9e080f724f
Joe Timmins <joe@novoda.com>
2017-04-02 11:54:18 +0100

    Remove unused Story classes

--- core/src/main/java/com/novoda/materialised/hackernews/asynclistview/DefaultViewModel.kt
+++ core/src/main/java/com/novoda/materialised/hackernews/asynclistview/DefaultViewModel.kt
@@ -4,5 +4,4 @@ package com.novoda.materialised.hackernews.asynclistview
 data class DefaultViewModel<T>(
         val viewData: T,
         val viewBehaviour: ClickListener<T>
-) {
-}
+)
--- core/src/main/java/com/novoda/materialised/hackernews/asynclistview/ViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.novoda.materialised.hackernews.asynclistview;
-
-public interface ViewModel<T extends ViewData> {
-    T viewData();
-
-    ClickListener<T> viewBehaviour();
-}
