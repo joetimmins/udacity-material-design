d5b098ae828f6e6c3ccc6b9818f39034d45fab33
Joe Timmins <joe@novoda.com>
2017-05-02 14:56:20 +0100

    Promoting onClick to ViewModel works correctly, test passes

--- core/src/main/java/com/novoda/materialised/hackernews/asynclistview/ViewModel.kt
+++ core/src/main/java/com/novoda/materialised/hackernews/asynclistview/ViewModel.kt
@@ -3,4 +3,8 @@ package com.novoda.materialised.hackernews.asynclistview
 data class ViewModel<T : ViewData<Any>>(
         val viewData: T,
         val viewBehaviour: ClickListener<T>
-)
+) {
+    fun onClick() {
+        viewBehaviour.onClick(viewData)
+    }
+}
