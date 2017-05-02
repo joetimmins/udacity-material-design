da9c0963d59c705651db8458a134524c36eb9809
Joe Timmins <joe@novoda.com>
2017-05-02 14:11:33 +0100

    remove unnecessary interface

--- core/src/main/java/com/novoda/materialised/hackernews/section/Section.kt
+++ core/src/main/java/com/novoda/materialised/hackernews/section/Section.kt
@@ -1,12 +1,10 @@
 package com.novoda.materialised.hackernews.section
 
-import com.novoda.materialised.hackernews.asynclistview.ViewData
-
 
 enum class Section(
-        override val id: String,
+        val id: String,
         val userFacingName: String
-) : ViewData<String> {
+) {
     TOP_STORIES("topstories", "Top Stories"),
     NEW("newstories", "New"),
     BEST("beststories", "Best")
