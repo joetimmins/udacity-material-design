44b06f53653eda3ad3ef304755823d49b3829f2a
Joe Timmins <joe@novoda.com>
2017-05-02 19:29:36 +0100

    Add TabView

---
+++ core/src/main/java/com/novoda/materialised/hackernews/section/view/TabView.kt
@@ -0,0 +1,8 @@
+package com.novoda.materialised.hackernews.section.view
+
+import com.novoda.materialised.hackernews.asynclistview.ViewData
+import com.novoda.materialised.hackernews.asynclistview.ViewModel
+
+interface TabView<T : ViewData<String>> {
+    fun updateWith(viewModels: List<ViewModel<T>>)
+}
