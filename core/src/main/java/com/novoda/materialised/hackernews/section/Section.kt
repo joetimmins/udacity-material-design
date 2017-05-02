fddcd31de30273bb448b92497b9be64c6c9d5ec6
Joe Timmins <joe@novoda.com>
2017-05-02 19:50:53 +0100

    Make Section implement ViewData

--- core/src/main/java/com/novoda/materialised/hackernews/section/Section.kt
+++ core/src/main/java/com/novoda/materialised/hackernews/section/Section.kt
@@ -1,9 +1,11 @@
 package com.novoda.materialised.hackernews.section
 
+import com.novoda.materialised.hackernews.asynclistview.ViewData
+
 enum class Section(
-        val id: String,
+        override val id: String,
         val userFacingName: String
-) {
+) : ViewData<String> {
     TOP_STORIES("topstories", "Top Stories"),
     NEW("newstories", "New"),
     BEST("beststories", "Best")
---
+++ core/src/test/java/com/novoda/materialised/hackernews/section/SectionListPresenterTest.kt
@@ -0,0 +1,13 @@
+package com.novoda.materialised.hackernews.section
+
+import org.junit.Test
+
+
+class SectionListPresenterTest {
+    @Test
+    fun populateViewWithViewModelsFromProvider_whenPresenting() {
+
+
+
+    }
+}
