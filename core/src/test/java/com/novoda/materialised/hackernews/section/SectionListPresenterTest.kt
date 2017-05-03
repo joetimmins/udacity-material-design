d8e3af3aa213a8aa5631cbb314750098f758e61a
Joe Timmins <joe@novoda.com>
2017-05-03 10:43:00 +0100

    Added failing test for section list presenter

--- core/src/test/java/com/novoda/materialised/hackernews/section/SectionListPresenterTest.kt
+++ core/src/test/java/com/novoda/materialised/hackernews/section/SectionListPresenterTest.kt
@@ -1,13 +1,27 @@
 package com.novoda.materialised.hackernews.section
 
+import com.novoda.materialised.hackernews.asynclistview.ViewModel
+import com.novoda.materialised.hackernews.section.view.TabView
+import org.fest.assertions.api.Assertions.assertThat
 import org.junit.Test
 
 
 class SectionListPresenterTest {
+
     @Test
     fun populateViewWithViewModelsFromProvider_whenPresenting() {
+        val view = SpyingTabView()
 
-
-
+        val expectedList: List<ViewModel<Section>> = listOf(ViewModel(Section.TOP_STORIES), ViewModel(Section.BEST))
+        assertThat(view.receivedList).isEqualTo(expectedList)
     }
+}
+
+class SpyingTabView : TabView<Section> {
+    var receivedList: List<ViewModel<Section>> = emptyList()
+
+    override fun updateWith(viewModels: List<ViewModel<Section>>) {
+        receivedList = viewModels
+    }
+
 }
