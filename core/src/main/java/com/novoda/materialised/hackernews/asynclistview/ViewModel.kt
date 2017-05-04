4cd7d9a5087620c43a75964f92b843ef0636b284
Joe Timmins <joe@novoda.com>
2017-05-04 10:45:43 +0100

    Suppress variance modifier

--- core/src/main/java/com/novoda/materialised/hackernews/asynclistview/ViewModel.kt
+++ core/src/main/java/com/novoda/materialised/hackernews/asynclistview/ViewModel.kt
@@ -1,5 +1,6 @@
 package com.novoda.materialised.hackernews.asynclistview
 
+@Suppress("AddVarianceModifier") // adding the variance modifier adds too much noise on the Java side
 data class ViewModel<T : ViewData<Any>>(
         val viewData: T,
         private val viewBehaviour: (T) -> Unit = {}
