c2a01eac6212835fe1399ba0e15d24b6b9c56d2b
Joe Timmins <joe@novoda.com>
2017-05-23 14:38:28 +0100

    Use an Rx pipeline to create story provider

--- mobile/src/main/java/com/novoda/materialised/hackernews/stories/provider/FirebaseStoryProvider.java
+++ mobile/src/main/java/com/novoda/materialised/hackernews/stories/provider/FirebaseStoryProvider.java
@@ -4,13 +4,15 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.jetbrains.annotations.NotNull;
 
 import io.reactivex.Observable;
-import io.reactivex.Single;
+import io.reactivex.ObservableSource;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.BiFunction;
+import io.reactivex.functions.Function;
 import kotlin.jvm.functions.Function1;
 
 final class FirebaseStoryProvider implements StoryProvider {
@@ -23,23 +25,43 @@ final class FirebaseStoryProvider implements StoryProvider {
     @NotNull
     @Override
     public Observable<Story> readItems(@NotNull List<Integer> ids) {
-        DatabaseReference databaseReference = firebaseDatabase.getReference("v0").child("item");
-        Function1<DataSnapshot, Story> converter = new Function1<DataSnapshot, Story>() {
+        final DatabaseReference databaseReference = firebaseDatabase.getReference("v0").child("item");
+        final Function1<DataSnapshot, Story> converter = new Function1<DataSnapshot, Story>() {
             @Override
             public Story invoke(DataSnapshot dataSnapshot) {
                 return dataSnapshot.getValue(Story.class);
             }
         };
 
-        Observable<Story> result = Observable.empty();
-        List<Single<Story>> storySingles = new ArrayList<>(ids.size());
-        for (Integer id : ids) {
-            DatabaseReference childReference = databaseReference.child(Integer.toString(id));
-            storySingles.add(FirebaseSingleEventListener.listen(childReference, converter));
-        }
-        for (Single<Story> storySingle : storySingles) {
-            result = result.mergeWith(storySingle.toObservable());
-        }
-        return result;
+//        Observable<Story> result = Observable.empty();
+//        List<Single<Story>> storySingles = new ArrayList<>(ids.size());
+//        for (Integer id : ids) {
+//            DatabaseReference childReference = databaseReference.child(Integer.toString(id));
+//            storySingles.add(FirebaseSingleEventListener.listen(childReference, converter));
+//        }
+//        for (Single<Story> storySingle : storySingles) {
+//            result = result.mergeWith(storySingle.toObservable());
+//        }
+//        return result;
+        return Observable.fromIterable(ids)
+                .map(new Function<Integer, Observable<Story>>() {
+                    @Override
+                    public Observable<Story> apply(@NonNull Integer id) throws Exception {
+                        DatabaseReference childReference = databaseReference.child(Integer.toString(id));
+                        return FirebaseSingleEventListener.listen(childReference, converter).toObservable();
+                    }
+                })
+                .reduce(new BiFunction<Observable<Story>, Observable<Story>, Observable<Story>>() {
+                    @Override
+                    public Observable<Story> apply(@NonNull Observable<Story> storyObservable, @NonNull Observable<Story> nextStoryObservable) throws Exception {
+                        return storyObservable.mergeWith(nextStoryObservable);
+                    }
+                })
+                .flatMapObservable(new Function<Observable<Story>, ObservableSource<? extends Story>>() {
+                    @Override
+                    public ObservableSource<? extends Story> apply(@NonNull Observable<Story> storyObservable) throws Exception {
+                        return storyObservable;
+                    }
+                });
     }
 }
