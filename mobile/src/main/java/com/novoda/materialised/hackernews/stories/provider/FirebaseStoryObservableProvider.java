24dcd289212bb0d98366678856dce0a8859fd51b
Joe Timmins <joe@novoda.com>
2017-05-23 22:04:22 +0100

    Add story observable provider, along with its ridiculous test

--- mobile/src/main/java/com/novoda/materialised/hackernews/stories/provider/FirebaseStoryObservableProvider.java
+++ mobile/src/main/java/com/novoda/materialised/hackernews/stories/provider/FirebaseStoryObservableProvider.java
@@ -1,11 +1,39 @@
 package com.novoda.materialised.hackernews.stories.provider;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Function;
+import kotlin.jvm.functions.Function1;
 
 class FirebaseStoryObservableProvider {
     private final FirebaseDatabase firebaseDatabase;
 
-    public FirebaseStoryObservableProvider(FirebaseDatabase firebaseDatabase) {
+    FirebaseStoryObservableProvider(FirebaseDatabase firebaseDatabase) {
         this.firebaseDatabase = firebaseDatabase;
+    }
+
+    Observable<Observable<Story>> createStoryObservables(List<Integer> storyIds) {
+        final DatabaseReference databaseReference = firebaseDatabase.getReference("v0").child("item");
+        final Function1<DataSnapshot, Story> converter = new Function1<DataSnapshot, Story>() {
+            @Override
+            public Story invoke(DataSnapshot dataSnapshot) {
+                return dataSnapshot.getValue(Story.class);
+            }
+        };
+
+        return Observable.fromIterable(storyIds)
+                .map(new Function<Integer, Observable<Story>>() {
+                    @Override
+                    public Observable<Story> apply(@NonNull Integer id) throws Exception {
+                        DatabaseReference childReference = databaseReference.child(Integer.toString(id));
+                        return FirebaseSingleEventListener.listen(childReference, converter).toObservable();
+                    }
+                });
     }
 }
--- mobile/src/test/java/com/novoda/materialised/hackernews/stories/provider/FirebaseStoryObservableProviderTest.java
+++ mobile/src/test/java/com/novoda/materialised/hackernews/stories/provider/FirebaseStoryObservableProviderTest.java
@@ -1,27 +1,62 @@
 package com.novoda.materialised.hackernews.stories.provider;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+
+import static org.fest.assertions.api.Assertions.assertThat;
 
 public class FirebaseStoryObservableProviderTest {
 
     @Test
     public void readItemsCallsBackOncePerValueRetrieved() {
-//        TestObserver<Observable<Story>> storyObserver = new TestObserver<>();
-//        int firstStoryId = 12;
-//        int secondStoryId = 34;
-//        Story firstStory = new Story("author", 890, firstStoryId, Arrays.asList(1, 2), 4, 1232, "test title", "test type", "http://test.url");
-//        Story secondStory = new Story("another author", 567, secondStoryId, Arrays.asList(3, 4), 5, 7897, "another title", "another type", "http://another.url");
-//        List<Story> stories = Arrays.asList(firstStory, secondStory);
-//        FirebaseStoryObservableProvider provider = new FirebaseStoryObservableProvider(FakeFirebase.getItemsDatabase(stories));
-//
-//        List<Integer> storyIds = Arrays.asList(firstStoryId, secondStoryId);
-//        Observable<Story> observable = provider
-//                .createStoryObservables(storyIds)
-//                .subscribeOn(Schedulers.trampoline())
-//                .observeOn(Schedulers.trampoline());
-//
-//        observable.subscribe(storyObserver);
-//
-//        storyObserver.assertValues(firstStory, secondStory);
+        int firstStoryId = 12;
+        int secondStoryId = 34;
+        Story firstStory = new Story("author", 890, firstStoryId, Arrays.asList(1, 2), 4, 1232, "test title", "test type", "http://test.url");
+        Story secondStory = new Story("another author", 567, secondStoryId, Arrays.asList(3, 4), 5, 7897, "another title", "another type", "http://another.url");
+        List<Story> stories = Arrays.asList(firstStory, secondStory);
+        FirebaseStoryObservableProvider provider = new FirebaseStoryObservableProvider(FakeFirebase.getItemsDatabase(stories));
+
+        List<Integer> storyIds = Arrays.asList(firstStoryId, secondStoryId);
+        Observable<Observable<Story>> storyObservable = provider
+                .createStoryObservables(storyIds);
+
+        final List<Story> actualStories = new ArrayList<>();
+        storyObservable.subscribe(new OnNextObserver<Observable<Story>>() {
+            @Override
+            public void onNext(@NonNull Observable<Story> storyObservable) {
+                storyObservable.subscribe(new OnNextObserver<Story>() {
+                    @Override
+                    public void onNext(@NonNull Story story) {
+                        actualStories.add(story);
+                    }
+                });
+            }
+        });
+        assertThat(actualStories).isEqualTo(stories);
+    }
+
+    private static abstract class OnNextObserver<T> implements Observer<T> {
+        @Override
+        public final void onSubscribe(@NonNull Disposable d) {
+
+        }
+
+        @Override
+        public final void onError(@NonNull Throwable e) {
+
+        }
+
+        @Override
+        public final void onComplete() {
+
+        }
     }
 }
