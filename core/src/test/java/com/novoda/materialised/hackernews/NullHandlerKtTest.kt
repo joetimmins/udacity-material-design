12f3bc28a20a8af933f8f297e7b1b0bd3f5cbd62
Joe Timmins <joe@novoda.com>
2017-09-26 15:41:09 +0100

    Rename test methods

--- core/src/test/java/com/novoda/materialised/hackernews/NullHandlerKtTest.kt
+++ core/src/test/java/com/novoda/materialised/hackernews/NullHandlerKtTest.kt
@@ -8,21 +8,21 @@ private val defaultValue = 236
 class NullHandlerKtTest {
 
     @Test
-    fun returnsDefaultValue_WhenFunctionReturnsNull() {
+    fun returnDefaultValue_WhenFunctionReturnsNull() {
         val result = handleNullable({ null }, defaultValue)
 
         assertThat(result).isEqualTo(defaultValue)
     }
 
     @Test
-    fun returnsDefaultValue_WhenFunctionThrowsException() {
+    fun returnDefaultValue_WhenFunctionThrowsException() {
         val result = handleNullable({ throw RuntimeException() }, defaultValue)
 
         assertThat(result).isEqualTo(defaultValue)
     }
 
     @Test
-    fun returnsResultOFunction_WhenNotNull() {
+    fun returnResultOFunction_WhenNotNull() {
         val expected = 12
         val result = handleNullable({ expected }, defaultValue)
         assertThat(result).isEqualTo(expected)
