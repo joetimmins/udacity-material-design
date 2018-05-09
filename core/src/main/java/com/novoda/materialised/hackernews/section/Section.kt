6786083e093811de29fd0713dd3e7dbd1627f1eb
Joe Timmins <joe@novoda.com>
2018-05-09 17:23:25 +0100

    remove unused boolean

--- core/src/main/java/com/novoda/materialised/hackernews/section/Section.kt
+++ core/src/main/java/com/novoda/materialised/hackernews/section/Section.kt
@@ -4,10 +4,9 @@ import com.novoda.materialised.hackernews.asynclistview.ViewData
 
 enum class Section(
         override val id: String,
-        val userFacingName: String,
-        val isDefault: Boolean
+        val userFacingName: String
 ) : ViewData<String> {
-    TOP_STORIES("topstories", "Top Stories", true),
-    NEW("newstories", "New", false),
-    BEST("beststories", "Best", false)
+    TOP_STORIES("topstories", "Top Stories"),
+    NEW("newstories", "New"),
+    BEST("beststories", "Best")
 }
