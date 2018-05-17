b47777b8cfa476fd94cb8ab5d160079946623f38
Joe Timmins <joe@novoda.com>
2018-05-17 14:06:08 +0100

    Add note to TODO

--- core/src/main/java/com/novoda/materialised/hackernews/stories/provider/StoryProvider.kt
+++ core/src/main/java/com/novoda/materialised/hackernews/stories/provider/StoryProvider.kt
@@ -9,6 +9,7 @@ class StoryProvider(
 
     fun readItems(ids: List<Int>): Observable<Story> {
         // TODO filter out items that don't have type == STORY to clean up the logcat output if nothing else
+        // or maybe not, that would need another class to deserialise to
         return Observable.fromIterable(ids)
                 .flatMap { id ->
                     itemsDatabase.child(id.toString()).singleValueOf(Story::class.java).toObservable()
