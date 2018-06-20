e5d62abe0e16c13efb0ac5d7f34cf359e594041e
Joe Timmins <joe@novoda.com>
2018-06-20 14:00:09 +0100

    Explain why we can't use a data class in for db structure

--- core/src/main/kotlin/com/novoda/materialised/hackernews/remotedb/RemoteDatabaseStructure.kt
+++ core/src/main/kotlin/com/novoda/materialised/hackernews/remotedb/RemoteDatabaseStructure.kt
@@ -1,3 +1,4 @@
 package com.novoda.materialised.hackernews.remotedb
 
+// this can't be a data class because data classes can't have varargs in the constructor
 class RemoteDatabaseStructure(val firstChildId: String, vararg val childIds: String)
