f60f622574c205bb83af07c8a1a6157f99cb8ede
Joe Timmins <joe@novoda.com>
2019-02-25 13:43:13 +0000

    Use an anonymous extension function; add equals and hashCode

--- remotedb/src/main/java/com/novoda/remotedb/Node.kt
+++ remotedb/src/main/java/com/novoda/remotedb/Node.kt
@@ -14,19 +14,20 @@ class Node internal constructor(private val databaseReference: DatabaseReference
         return Node(child)
     }
 
-    fun <T> singleValueOf(returnClass: Class<T>): Single<T> = databaseReference.toSingle { it.getValue(returnClass) }
+    fun <T> singleValueOf(returnClass: Class<T>): Single<T> = databaseReference.toSingle { getValue(returnClass) }
 
-    fun <T> singleListOf(returnClass: Class<T>): Single<List<T>> = databaseReference.toSingle { it.value as List<T> }
+    fun <T> singleListOf(returnClass: Class<T>): Single<List<T>> = databaseReference.toSingle { value as List<T> }
 
-    private fun <T> DatabaseReference.toSingle(convert: (DataSnapshot) -> T?) =
-            Single.create<T> { emitter ->
-                addListenerForSingleValueEvent(
-                        object : ValueEventListener {
-                            override fun onDataChange(data: DataSnapshot) = run { convert(data)?.let { emitter.onSuccess(it) } ?: Unit }
+    private fun <T> DatabaseReference.toSingle(toReturnType: DataSnapshot.() -> T?) =
+        Single.create<T> { emitter ->
+            addListenerForSingleValueEvent(
+                object : ValueEventListener {
+                    override fun onDataChange(data: DataSnapshot) =
+                        run { data.toReturnType()?.let { emitter.onSuccess(it) } ?: Unit }
 
-                            override fun onCancelled(error: DatabaseError) = run { emitter.onError(error.toException()) }
-                        }
-                )
-            }
-
+                    override fun onCancelled(error: DatabaseError) =
+                        run { emitter.onError(error.toException()) }
+                }
+            )
+        }
 }
--- remotedb/src/main/java/com/novoda/remotedb/Structure.kt
+++ remotedb/src/main/java/com/novoda/remotedb/Structure.kt
@@ -2,6 +2,25 @@ package com.novoda.remotedb
 
 // this can't be a data class because data classes can't have varargs in the constructor
 class Structure(
-        val firstChildId: String,
-        vararg val childIds: String
-)
+    val firstChildId: String,
+    vararg val childIds: String
+
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Structure
+
+        if (firstChildId != other.firstChildId) return false
+        if (!childIds.contentEquals(other.childIds)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = firstChildId.hashCode()
+        result = 31 * result + childIds.contentHashCode()
+        return result
+    }
+}
